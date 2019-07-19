1f86d8401f33d7b8ac76f980d2bca4758172271a
Stanislav Breussov <s.breussov@gmail.com>
2019-07-19 18:01:31 +0600

    Move keys

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 
 namespace Snake
@@ -11,14 +12,25 @@ namespace Snake
             HorizontalLine downLine = new HorizontalLine(0, 75, 22, '+');
             VerticalLine leftLine   = new VerticalLine(0, 22, 0, '+');
             VerticalLine rightLine  = new VerticalLine(0, 22, 75, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+            snake.Drow();
+           
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
         }
     }
 }
